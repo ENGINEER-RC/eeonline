4fd340907e9e990d82e2575003dcfb20ae966f76
Tomrock D'souza <ajmet22@gmail.com>
2017-06-14 18:57:45 +0530

    Removed unused javascript files

--- eeonline/javascript/mainjs.js
+++
@@ -1,5 +0,0 @@
-//This javascript is a script that blinks colors. 
-function gen_math(a,b){return Math.floor((Math.random() * b) + a);}
-function colorx(){return "rgb("+gen_math(220,255)+","+gen_math(0,180)+","+gen_math(0,180)+")";}
-setInterval(function(){document.getElementById("hellox").style.color=colorx();}, 100);
-
